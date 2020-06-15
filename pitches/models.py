2a04b23d5544ab9214406d602e7615ccad81e052
faaiz <faaizajaz@gmail.com>
2020-06-15 23:17:59 +0500

    A test GH commit

--- pitches/models.py
+++ pitches/models.py
@@ -5,7 +5,7 @@ from routes.models import Route
 class Pitch(models.Model):
     pgrade = models.ForeignKey(Grade, on_delete=models.DO_NOTHING, verbose_name='Pitch Grade')
     #pdescription is only shown on the 'pitch' template.
-    #if a route only has one pitch, their is no link to a pitch template.
+    #if a route only has one pitch, their is no link to a pitch template
     pdescription = models.TextField(verbose_name='Pitch Description', blank=True, default="")
     proute = models.ForeignKey(Route, on_delete=models.DO_NOTHING, verbose_name='Route', null=True)
     plength = models.FloatField(verbose_name='Pitch Length', default=0.0)
