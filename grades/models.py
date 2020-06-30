a1428043029ff6b0b9fc5583f3518744f7ca59fc
faaiz <faaizajaz@gmail.com>
2020-06-30 19:09:49 +0500

    Made grades lookup table (grades/fixtures/grades.json)

--- grades/models.py
+++ grades/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Grade(models.Model):
-    ggrade = models.CharField(max_length=500, verbose_name='Grade')
+    #ggrade is french grade, need to refactor
+    ggrade = models.CharField(max_length=10, verbose_name='French Grade', blank=True)
+    ydsgrade = models.CharField(max_length=10, verbose_name='YDS Grade', blank=True)
+    ausgrade = models.CharField(max_length=10, verbose_name='Ewbanks Grade', blank=True)
+    uiaagrade = models.CharField(max_length=10, verbose_name='UIAA Grade', blank=True)
+    sagrade = models.CharField(max_length=10, verbose_name='SA Grade', blank=True)
+    ukgrade = models.CharField(max_length=10, verbose_name='UK Grade', blank=True)
 
     def __str__(self):
     	return f'{self.ggrade}'
