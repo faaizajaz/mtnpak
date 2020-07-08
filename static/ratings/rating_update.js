61f58697af5aa7d2c32a56dd7f74dda63ca27bfa
faaiz <faaizajaz@gmail.com>
2020-07-08 23:30:35 +0500

    Star ratings working!

---
+++ static/ratings/rating_update.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+	$("#star5").click(function(e){
+	//$("input[name='rating']").click(function(e){
+		//e.preventDefault();
+		var this_ = $(this);
+		var rateUrl = this_.attr("data-api");
+		console.log(rateUrl)
+		$.ajax({
+			type: 'GET',
+			url: rateUrl,
+			data: {},
+			success: function(data){
+				$("#avg_score").html(data);
+
+			}, error: function(error){
+				console.log(error);
+				console.log("error");
+			}
+		});
+
+	});
+	$("#star4").click(function(e){
+	//$("input[name='rating']").click(function(e){
+		//e.preventDefault();
+		var this_ = $(this);
+		var rateUrl = this_.attr("data-api");
+		console.log(rateUrl)
+		$.ajax({
+			type: 'GET',
+			url: rateUrl,
+			data: {},
+			success: function(data){
+				$("#avg_score").html(data);
+
+			}, error: function(error){
+				console.log(error);
+				console.log("error");
+			}
+		});
+
+	});
+	$("#star3").click(function(e){
+	//$("input[name='rating']").click(function(e){
+		//e.preventDefault();
+		var this_ = $(this);
+		var rateUrl = this_.attr("data-api");
+		console.log(rateUrl)
+		$.ajax({
+			type: 'GET',
+			url: rateUrl,
+			data: {},
+			success: function(data){
+				$("#avg_score").html(data);
+
+			}, error: function(error){
+				console.log(error);
+				console.log("error");
+			}
+		});
+
+	});
+	$("#star2").click(function(e){
+	//$("input[name='rating']").click(function(e){
+		//e.preventDefault();
+		var this_ = $(this);
+		var rateUrl = this_.attr("data-api");
+		console.log(rateUrl)
+		$.ajax({
+			type: 'GET',
+			url: rateUrl,
+			data: {},
+			success: function(data){
+				$("#avg_score").html(data);
+
+			}, error: function(error){
+				console.log(error);
+				console.log("error");
+			}
+		});
+
+	});
+	$("#star1").click(function(e){
+	//$("input[name='rating']").click(function(e){
+		//e.preventDefault();
+		var this_ = $(this);
+		var rateUrl = this_.attr("data-api");
+		console.log(rateUrl)
+		$.ajax({
+			type: 'GET',
+			url: rateUrl,
+			data: {},
+			success: function(data){
+				$("#avg_score").html(data);
+
+			}, error: function(error){
+				console.log(error);
+				console.log("error");
+			}
+		});
+
+	});
+});
