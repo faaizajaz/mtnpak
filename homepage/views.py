61004279d69a603d0ebde3a73a3a70d30ba6b74e
faaiz <faaizajaz@gmail.com>
2020-07-15 21:11:26 +0500

    Added leaderboard and carousel to homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from routes.models import Route
 
 
 def HomePage(request):
     # return HttpResponse('<h1>MTNPAK home page')
 
+    top_routes = Route.objects.all().order_by('-avg_rating')[:10]
 
-    return render(request, 'homepage/homepage.html')
+    context = {'top_routes': top_routes}
+
+
+    return render(request, 'homepage/homepage.html', context)
