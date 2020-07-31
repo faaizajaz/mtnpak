d5eacbcfbea02b7a2d7149c3da12aa2e6b0257ca
faaiz <faaizajaz@gmail.com>
2020-07-31 22:54:24 +0500

    Stuff

--- routes/models.py
+++ routes/models.py
@@ -7,7 +7,7 @@ from comments.models import Comment
 
 class Route(models.Model):
     rname = models.CharField(max_length=500, verbose_name='Route Name')
-    rdescription = models.TextField(verbose_name='Route Description')
+    rdescription = models.CharField(max_length=10000, verbose_name='Route Description')
     ropener = models.CharField(max_length=500, verbose_name='Route Opener')
     rcrag = models.ForeignKey(Crag, on_delete=models.CASCADE, null=True, verbose_name='Crag')
     #In case of multipitch, the highest grade of all pitches
