32986e346d564e98ca3c4bb6e629ae151ac1201b
faaiz <faaizajaz@gmail.com>
2020-08-12 23:49:31 +0500

    improved comments.js portability, now just requires .comment-form on any model view page

--- static/comments/comments.js
+++ static/comments/comments.js
@@ -1,30 +1,17 @@
 $(document).ready(function(){
-	commentUrl = $(".route-comment-form").attr("data-api");
+	commentUrl = $(".comment-form").attr("data-api");
 	
-	$(".route-comment-form").ajaxForm({
+	$(".comment-form").ajaxForm({
 		url: commentUrl, 
 		type: "post",
 		success: function(data){
 			$( ".comment-container" ).load(window.location.href + " .comment-container" );
-			$(".route-comment-form").clearForm();
+			$(".comment-form").clearForm();
 		}
 	});
 
 })
 
-$(document).ready(function(){
-	commentUrl = $(".crag-comment-form").attr("data-api");
-	
-	$(".crag-comment-form").ajaxForm({
-		url: commentUrl, 
-		type: "post",
-		success: function(data){
-			$( ".comment-container" ).load(window.location.href + " .comment-container" );
-			$(".crag-comment-form").clearForm();
-		}
-	});
-
-})
 
 
 
