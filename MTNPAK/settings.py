5a81bbb380d470ab061d7927798c518f9886b902
faaiz <faaizajaz@gmail.com>
2020-08-14 14:01:24 +0500

    Switched to postgres db

--- MTNPAK/settings.py
+++ MTNPAK/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'MTNPAK.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mtnpak',
+        'USER': 'faaiz',
+        'PASSWORD': 'Outpo3t33',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
