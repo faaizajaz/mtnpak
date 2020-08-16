9f6027499ac355448d257e9a0229590dde753b63
faaiz <faaizajaz@gmail.com>
2020-08-16 18:32:01 +0500

    added password hasher

--- MTNPAK/settings.py
+++ MTNPAK/settings.py
@@ -137,6 +137,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
