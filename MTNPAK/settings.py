a088fa5cdad957c0fb013f38ad5a2c764c368892
faaiz <faaizajaz@gmail.com>
2020-08-16 18:09:43 +0500

    updted db in settings

--- MTNPAK/settings.py
+++ MTNPAK/settings.py
@@ -103,20 +103,20 @@ WSGI_APPLICATION = 'MTNPAK.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'mtnpak',
-       'USER': 'faaiz',
-       'PASSWORD': 'Outpo3t33',
-       'HOST': 'localhost',
-       'PORT': '5432', #this seems to be default that the server wants
-   }
+    'default': dj_database_url.config()
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mtnpak',
+#        'USER': 'faaiz',
+#        'PASSWORD': 'Outpo3t33',
+#        'HOST': 'localhost',
+#        'PORT': '5432', #this seems to be default that the server wants
+#    }
+# }
 
 
 # Password validation
