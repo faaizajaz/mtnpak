70bc36d206f4ec5f912cb335672c7d8e16270ee1
faaiz <faaizajaz@gmail.com>
2020-08-24 16:26:07 +0500

    Important security update to prevent link hijacking of user profile and prefs

--- users/views.py
+++ users/views.py
@@ -46,55 +46,63 @@ class UserProfile(LoginRequiredMixin, generic.DetailView):
 #This still does not check to see if current user is same as that to edit.
 @login_required
 def EditProfile(request, **kwargs):
+	# check if the current user is the same as the profile being edited, show form if true.
+	if request.user.username == kwargs['username']:
+		#if i do this like request.user.profile is it safer?		
+		p_instance = User.objects.get(username=kwargs['username']).profile
+		u_instance = User.objects.get(username=kwargs['username'])
 
-	#if i do this like request.user.profile is it safer?
+		if request.method == 'POST':
+			#we pass instances here to prepopulate forms with current.
+			user_form = UserUpdateForm(request.POST, instance=u_instance)
+			profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=p_instance)
+			if user_form.is_valid() and profile_form.is_valid():
+				user_form.save()
+				profile_form.save()
 
-	u_instance = User.objects.get(username=kwargs['username'])
-	p_instance = User.objects.get(username=kwargs['username']).profile
-
-	if request.method == 'POST':
-		#we pass instances here to prepopulate forms with current.
-		user_form = UserUpdateForm(request.POST, instance=u_instance)
-		profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=p_instance)
-		if user_form.is_valid() and profile_form.is_valid():
-			user_form.save()
-			profile_form.save()
-
-			#redirect using u_instance.username in case user changes username
-			return redirect('profile', username=u_instance.username)
+				#redirect using u_instance.username in case user changes username
+				return redirect('profile', username=u_instance.username)
+		else:
+			user_form = UserUpdateForm(instance=u_instance)
+			profile_form = ProfileUpdateForm(instance=p_instance)
+		return render(request, 'users/editprofile.html', {'user_form': user_form, 'profile_form': profile_form})
+	# this is if somebody manually goes to the edit route for a profile.
 	else:
-		user_form = UserUpdateForm(instance=u_instance)
-		profile_form = ProfileUpdateForm(instance=p_instance)
-	return render(request, 'users/editprofile.html', {'user_form': user_form, 'profile_form': profile_form})
+		print("Not the right user")
 
 
 @login_required
 def UpdateUserPrefs(request, **kwargs):
+	# check that current user is same as prefs being edited. 
+	if request.user.username == kwargs['username']:
 
-	instance = User.objects.get(username=kwargs['username']).userpref
+		instance = User.objects.get(username=kwargs['username']).userpref
 
-	if request.method == 'POST':
-		form = UpdateUserPrefsForm(request.POST, instance = instance)
-		if form.is_valid():
-			form.save()
+		if request.method == 'POST':
+			form = UpdateUserPrefsForm(request.POST, instance = instance)
+			if form.is_valid():
+				form.save()
 
-			#Set session variable for grade preference when prefs updated.
-			#Also done in users.signals.create_pref_session when session is created
-			request.session['grade_pref'] = request.user.userpref.grade_pref
-			request.session['measurement_pref'] = request.user.userpref.measurement_pref
+				#Set session variable for grade preference when prefs updated.
+				#Also done in users.signals.create_pref_session when session is created
+				request.session['grade_pref'] = request.user.userpref.grade_pref
+				request.session['measurement_pref'] = request.user.userpref.measurement_pref
 
 
-			# if timezone_pref is set, activate timezone, else use default.
-			if request.user.userpref.timezone_pref:
-				request.session['timezone_pref'] = str(request.user.userpref.timezone_pref)
-				timezone.activate(pytz.timezone(request.session['timezone_pref']))
-			else:
-				timezone.activate(pytz.timezone(settings.TIME_ZONE))
+				# if timezone_pref is set, activate timezone, else use default.
+				if request.user.userpref.timezone_pref:
+					request.session['timezone_pref'] = str(request.user.userpref.timezone_pref)
+					timezone.activate(pytz.timezone(request.session['timezone_pref']))
+				else:
+					timezone.activate(pytz.timezone(settings.TIME_ZONE))
 
-			# once saved, redirect to user profile.
-			return redirect('profile', username=kwargs['username'])
+				# once saved, redirect to user profile.
+				return redirect('profile', username=kwargs['username'])
 
-			
+				
+		else:
+			form = UpdateUserPrefsForm(instance = instance)
+		return render(request, 'users/editprefs.html', {'form': form})
+
 	else:
-		form = UpdateUserPrefsForm(instance = instance)
-	return render(request, 'users/editprefs.html', {'form': form})
+		print("wrong user")
